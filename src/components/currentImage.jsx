decae3b829c61ae0eba91a0244aa176ed78e22f3
Jeanette Yue <yue.jeanette@gmail.com>
2020-03-07 10:23:14 -0800

    moved caption to image component

--- src/components/currentImage.jsx
+++ src/components/currentImage.jsx
@@ -1,63 +1,23 @@
-import React, { useContext, useRef } from "react";
-import { Container } from "@material-ui/core";
-import ColorThief from "colorthief";
-import CloseButton from "./closeButton";
-import Image from "./image";
+import React, { useContext } from "react";
+import Container from "@material-ui/core/Container";
+
 import SelectionContext from "../context/selectionContext";
 
-const styles = {
-  container: {
-    height: "50vh",
-    maxWidth: "60%",
-    width: "auto",
-    textAlign: "center"
-  },
-  figure: {
-    height: "100%"
-  },
-  image: {
-    height: "80%"
-  }
-};
+import CloseButton from "./closeButton";
+import Image from "./image";
 
-// given the current img provided by context, use colorthief to find the
-// dominant colors and palette, then invoke eventhandler passed by parent to
-// update 'dominant' and 'palette' in ancestor's state
-const generatePalette = (ref, handleImageLoad) => {
-  const colorThief = new ColorThief();
-  const dominant = colorThief.getColor(ref);
-  const palette = colorThief.getPalette(ref);
-  handleImageLoad(dominant, palette);
-};
-
-// const handleImageLoad = (dominant, palette) => {
-//   console.log(dominant, palette);
-// }
-
-const CurrentImage = ({ handleImageLoad }) => {
+const CurrentImage = () => {
   const imageContext = useContext(SelectionContext);
-  const imgEl = useRef(null);
-  console.log(imageContext);
+  const [selectedImage, handleImageLoad] = [
+    imageContext.selectedImage,
+    imageContext.handleImageLoad
+  ];
 
   return (
-    // <Container style={{ ...styles.container }}>
     <Container style={{ width: "70%", height: "70%" }}>
       <CloseButton />
-      <figure style={{ ...styles.image }}>
-        <Image
-          src={imageContext.selectedImage}
-          handleLoad={imageContext.handleImageLoad}
-        />
-        {/* <img
-          src={imageContext.selectedImage}
-          style={{ ...styles.image }}
-          ref={imgEl}
-          onLoad={() => generatePalette(imgEl.current, handleImageLoad)}
-        /> */}
-        <figcaption>{imageContext.selectedImage}</figcaption>
-      </figure>
+      <Image src={selectedImage} handleLoad={handleImageLoad} />
     </Container>
-    // </Container>
   );
 };
 
--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,5 +1,5 @@
 /**
- * returns a responsive image wrapped in MUI container
+ * returns a responsive image with caption wrapped in MUI container
  * this is where the magic happens
  * when this image loads, invoke colothief methods to create dominant and palette colors, then update main state via prop
  */
@@ -7,6 +7,7 @@
 import React, { useRef } from "react";
 import ColorThief from "colorthief";
 import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 
 // given the current img provided by the image ref, use colorthief to find the
 // dominant colors and palette and save results to App.js state
@@ -19,13 +20,14 @@ const Image = ({ src, handleLoad }) => {
   const imgRef = useRef(null);
 
   return (
-    <Container style={{ width: "75%", height: "75%" }}>
+    <Container style={{ width: "75%", height: "75%", textAlign: "center" }}>
       <img
         src={src}
         ref={imgRef}
         onLoad={() => handleLoad(...generatePalette(imgRef.current))}
         style={{ maxWidth: "100%", maxHeight: "100%" }}
       />
+      <Typography variant="subtitle1">{src}</Typography>
     </Container>
   );
 };
