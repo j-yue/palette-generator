85cc9d3d9dc7e1c82c5246974cbc70e65866943a
Jeanette Yue <yue.jeanette@gmail.com>
2020-03-09 11:19:29 -0700

    placed api call inside useEffect hook to prevent mult api calls

--- src/components/contrastRatio.jsx
+++ src/components/contrastRatio.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CheckIcon from "@material-ui/icons/Check";
 import ClearIcon from "@material-ui/icons/Clear";
 import Container from "@material-ui/core/Container";
@@ -6,24 +6,27 @@ import withGrid from "./hoc/withGrid";
 
 //get ratio from webAIM api and render the result
 const ContrastRatio = ({ fg, bg }) => {
-  const [ratio, setRatio] = useState("ratio");
-  // const [pass, setPass] = useState("pass");
-  const [icon, setIcon] = useState(<CheckIcon />);
+  const [ratio, setRatio] = useState(null);
+  const [icon, setIcon] = useState(null);
   const URL = `https://webaim.org/resources/contrastchecker/?fcolor=${fg}&bcolor=${bg}&api`;
 
-  fetch(URL)
-    .then(response => response.json())
-    .then(({ ratio, AA }) => {
-      setRatio(ratio);
-      AA === "pass" ? setIcon(<CheckIcon />) : setIcon(<ClearIcon />);
-    });
-
-  // const icon =
+  useEffect(() => {
+    try {
+      fetch(URL)
+        .then(response => response.json())
+        .then(({ ratio, AA }) => {
+          setRatio(ratio);
+          AA === "pass" ? setIcon(<CheckIcon />) : setIcon(<ClearIcon />);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }, [fg, bg]);
 
   return (
     <React.Fragment>
       {icon}
-      <div>{ratio}</div>
+      <div style={{ fontSize: "4rem" }}>{ratio}</div>
     </React.Fragment>
   );
 };
